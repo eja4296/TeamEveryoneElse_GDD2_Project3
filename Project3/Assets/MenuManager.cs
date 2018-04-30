9fc90c9d722e3e7c50860b330099b91583eb62b3
Joey Tong <joeyptong@yahoo.com>
2018-04-30 11:42:19 -0400

    in game menu

---
+++ Project3/Assets/MenuManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+    public GameObject MenuContainer;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            MenuContainer.SetActive(!MenuContainer.activeSelf);
+            if (MenuContainer.activeSelf) {
+                Time.timeScale = 0f;
+            }else {
+                Time.timeScale = 1f;
+            }
+            
+        }
+        
+	}
+    public void Resume() {
+        MenuContainer.SetActive(false);
+        Time.timeScale = 1f;
+    }
+    public void MainMenu() {
+        SceneManager.LoadScene("Main Menu");
+    }
+    public void Exit() {
+        Application.Quit();
+    }
+}
